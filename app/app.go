8ee4255515e633b5ac7f526f7bdee6756d5e4f49
Yojahuang <yojahuang30613@gmail.com>
2022-02-20 00:32:47 +0800

    Add app handler

---
+++ app/app.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+    "log"
+    "net/http"
+)
+
+import (
+    "github.com/gorilla/mux"
+)
+
+import (
+    "gorm.io/driver/sqlite"
+    "gorm.io/gorm"
+)
+
+import (
+    "dcard-backend-hw/model"
+    "dcard-backend-hw/handler"
+)
+
+type requestHandlerFunction func(db *gorm.DB, w http.ResponseWriter, r *http.Request)
+
+func requestFuncWrapper(db *gorm.DB, handler requestHandlerFunction) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        handler(db, w, r)
+    }
+}
+
+func Init() {
+    log.Println("Connecting to database.db...")
+    db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+    log.Println("Done")
+
+    log.Println("Migrating database.db...")
+    db.AutoMigrate(&model.Url{})
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Done")
+
+    router := mux.NewRouter()
+
+    router.HandleFunc("/api/v1/urls", requestFuncWrapper(db, handler.UploadURL)).Methods("POST")
+
+    log.Println("API is running on port 4000!")
+    log.Fatal(http.ListenAndServe(":4000", router))
+}
