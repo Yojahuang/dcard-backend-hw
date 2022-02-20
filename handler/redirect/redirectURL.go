d8f4673c519e52214ab24e9ca212e8b7761afc72
Yojahuang <yojahuang30613@gmail.com>
2022-02-20 16:18:12 +0800

    Add expire detection feature

---
+++ handler/redirect/redirectURL.go
@@ -0,0 +1,58 @@
+package redirectHandler
+
+import (
+    "log"
+    "net/http"
+    "strconv"
+    "time"
+)
+
+import (
+    "github.com/gorilla/mux"
+)
+
+import (
+    "gorm.io/gorm"
+)
+
+import (
+    "dcard-backend-hw/model"
+)
+
+func hasExpired(url model.Url) bool {
+    exp, err := time.Parse(time.RFC3339, url.ExpireAt)
+    if err != nil {
+        log.Fatal(err)
+        return false
+    }
+
+    now := time.Now()
+    log.Println(exp, now)
+    if now.After(exp) {
+        log.Println("dsfjidwfjierw")
+		return true
+    }
+
+    return false
+}
+
+func RedirectURL(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+    _ = db
+    vars := mux.Vars(r)
+
+    tmp, _ := strconv.Atoi(vars["id"])
+    Id := uint(tmp)
+
+    var url model.Url
+    result := db.Find(&url, Id)
+
+    if result.RowsAffected == 0 || hasExpired(url) {
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(http.StatusText(http.StatusNotFound)))
+        if result.RowsAffected != 0 {
+            db.Delete(&url)
+        }
+        return
+    }
+
+}
