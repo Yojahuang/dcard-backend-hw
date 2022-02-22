cd1e28e369326e31c59302b68c1e43f6ef8af537
Yojahuang <yojahuang30613@gmail.com>
2022-02-22 19:28:09 +0800

    Since case is a keyword in golang, so change it into testcase.

--- handler/redirect/hasExpired_test.go
+++ handler/redirect/hasExpired_test.go
@@ -27,27 +27,27 @@ func getTime(after int) string {
 
 
 func TestHasExpired(t *testing.T) {
-    type Case struct {
+    type testCase struct {
         arg model.Url
         expected bool
     }
 
-    cases := [] Case {
-        Case{arg: model.Url{ExpireAt: getTime(1)}, expected: false} ,
-        Case{arg: model.Url{ExpireAt: getTime(2)}, expected: false} ,
-        Case{arg: model.Url{ExpireAt: getTime(3)}, expected: false} ,
-        Case{arg: model.Url{ExpireAt: getTime(4)}, expected: false} ,
-        Case{arg: model.Url{ExpireAt: getTime(5)}, expected: false} ,
-        Case{arg: model.Url{ExpireAt: getTime(-1)}, expected: true} ,
-        Case{arg: model.Url{ExpireAt: getTime(-2)}, expected: true} ,
-        Case{arg: model.Url{ExpireAt: getTime(-3)}, expected: true} ,
-        Case{arg: model.Url{ExpireAt: getTime(-4)}, expected: true} ,
-        Case{arg: model.Url{ExpireAt: getTime(-5)}, expected: true} ,
+    testCases := [] testCase {
+        testCase{arg: model.Url{ExpireAt: getTime(1)}, expected: false} ,
+        testCase{arg: model.Url{ExpireAt: getTime(2)}, expected: false} ,
+        testCase{arg: model.Url{ExpireAt: getTime(3)}, expected: false} ,
+        testCase{arg: model.Url{ExpireAt: getTime(4)}, expected: false} ,
+        testCase{arg: model.Url{ExpireAt: getTime(5)}, expected: false} ,
+        testCase{arg: model.Url{ExpireAt: getTime(-1)}, expected: true} ,
+        testCase{arg: model.Url{ExpireAt: getTime(-2)}, expected: true} ,
+        testCase{arg: model.Url{ExpireAt: getTime(-3)}, expected: true} ,
+        testCase{arg: model.Url{ExpireAt: getTime(-4)}, expected: true} ,
+        testCase{arg: model.Url{ExpireAt: getTime(-5)}, expected: true} ,
     }
 
-    for i := 0; i < len(cases) ; i = i + 1 {
-        if hasExpired(cases[i].arg) != cases[i].expected {
-            t.Errorf("%s expect got %v, but got %v", cases[i].arg.ExpireAt, cases[i].expected, !cases[i].expected)
+    for _, testCase := range testCases {
+        if hasExpired(testCase.arg) != testCase.expected {
+            t.Errorf("%s expect got %v, but got %v", testCase.arg.ExpireAt, testCase.expected, !testCase.expected)
         }
     }
 }
